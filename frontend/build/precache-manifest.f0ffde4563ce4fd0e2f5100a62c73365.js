ed0fd345164c2febf21fee4a45388fc00776b38e
Shorya-agarwal <91627681+Shorya-agarwal@users.noreply.github.com>
2022-03-14 11:52:39 +0530

    Ready to deploy build was already successful

---
+++ frontend/build/precache-manifest.f0ffde4563ce4fd0e2f5100a62c73365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698a8a82067dffdc11b2a336ecb20a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7747a8d8635e84a1ab1",
+    "url": "/static/css/main.773d9120.chunk.css"
+  },
+  {
+    "revision": "2d57899b70b5b6555aa2",
+    "url": "/static/js/2.e1eb8106.chunk.js"
+  },
+  {
+    "revision": "7c3782a7e65f48d3b45b3ebb61610f23",
+    "url": "/static/js/2.e1eb8106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7747a8d8635e84a1ab1",
+    "url": "/static/js/main.0da03393.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
